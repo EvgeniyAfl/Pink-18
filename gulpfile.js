dc81a313fa9be4c51f385ac5f535f73c97b8f0de
Evgeniy <47885510+EvgeniyAfl@users.noreply.github.com>
2020-01-08 20:27:32 +0300

    Правки по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,8 @@ gulp.task("sprite", function () {
   gulp.task("copy", function () {
     return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
+    // "source/img/**",
+    "source/img/*.{png,jpg,webp}",
     "source/js/**",
     "source/*.ico"
     ], {
